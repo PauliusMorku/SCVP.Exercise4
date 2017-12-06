dbb870223831827e5fb16ee5b669ba9c3aad7e30
PauliusMorku <paulius.morku@gmail.com>
2017-12-06 17:10:51 +0100

    First part

--- main.cpp
+++ main.cpp
@@ -1,19 +1,96 @@
 #include <systemc.h>
+#include <iostream>
 
 // Place Interface:
-// TODO
+//template <class T>
+class placeInterface : public sc_interface
+{
+public:
+    virtual void addTokens(unsigned int n) = 0;
+    virtual void removeTokens(unsigned int n) = 0;
+    virtual unsigned int testTokens(void) = 0;
+};
 
 // Place Channel:
-// TODO
+class place : public placeInterface
+{
+public:
+    void addTokens(unsigned int n){tokens = tokens + n;}
+    void removeTokens(unsigned int n){tokens = tokens - n;}
+    unsigned int testTokens(void){return tokens;}
+
+    place(unsigned int n) {tokens = n;}
+
+private:
+    unsigned int tokens;
+};
 
 // Transition:
-// TODO
+template<unsigned int N=1, unsigned int M=1>
+SC_MODULE(transition)
+{
+public:
+    sc_port<placeInterface> in;
+    sc_port<placeInterface> out;
+
+    void fire(void)
+    {
+
+        if (in->testTokens() > 0)
+        {
+            std::cout << this->name() << ": Fired" << std::endl;
+            in->removeTokens(1);
+        }
+        else
+        {
+            std::cout << this->name() << ": NOT Fired" << std::endl;
+            out->addTokens(1);
+        }
+    }
+
+    SC_CTOR(transition)
+    {
+
+    }
+
+};
 
 // TOPLEVEL
+SC_MODULE(toplevel)
+{
+    public:
+    transition<1,1> t1, t2;
+    place p1, p2;
+
+    SC_CTOR(toplevel) : t1("t1"), t2("t2"), p1(1), p2(0)
+    {
+        SC_THREAD(process);
+        t1.in.bind(p1);
+        t1.out.bind(p2);
+        t2.in.bind(p2);
+        t2.out.bind(p1);
+    }
+
+    void process()
+    {
+        while(true)
+        {
+            wait(10,SC_NS);
+            t1.fire();
+            wait(10,SC_NS);
+            t1.fire();
+            wait(10,SC_NS);
+            t2.fire();
+            sc_stop();
+        }
+    }
+};
 
 int sc_main(int argc, char* argv[])
 {
-    // TODO
+    toplevel t("top");
+
+
 
     sc_start();
     return 0;
