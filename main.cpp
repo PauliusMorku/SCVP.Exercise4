254bc857aa6bb130ffb273da6860c9f189ed8ab3
PauliusMorku <paulius.morku@gmail.com>
2017-12-09 14:06:15 +0100

    Single Memory Bank implementation

--- main.cpp
+++ main.cpp
@@ -77,8 +77,7 @@ SC_MODULE(toplevel)
     transition<1,2> t1; // <?,?> passes parameters to module, <> leaves default parameters
     transition<2,1> t2;
     transition<1,1> t3;
-    place<1,1>  p1, p2, p3, p4;
-
+    place<1,1> p1, p2, p3, p4;
 
     SC_CTOR(toplevel) : t1("t1"), t2("t2"), t3("t3"), p1(1), p2(0), p3(0), p4(0)
     {
@@ -93,6 +92,7 @@ SC_MODULE(toplevel)
         t3.in.bind(p3);
         t3.out.bind(p4);
     }
+
     void process()
     {
         while(true)
@@ -108,12 +108,58 @@ SC_MODULE(toplevel)
             sc_stop();
         }
     }
+};
 
+// Memory Bank
+SC_MODULE(memorybank)
+{
+    public:
+    transition<1,1> ACT;
+    transition<1,1> RD;
+    transition<1,1> PRE;
+    transition<1,1> WR;
+    place<1,1> IDLE;
+    place<3,3> ACTIVE;
+
+    SC_CTOR(memorybank) : ACT("ACT"), RD("RD"), PRE("PRE"), WR("WR"), IDLE(1), ACTIVE(0)
+    {
+        SC_THREAD(process);
+
+        ACT.in.bind(IDLE);
+        ACT.out.bind(ACTIVE);
+        RD.in.bind(ACTIVE);
+        RD.out.bind(ACTIVE);
+        PRE.in.bind(ACTIVE);
+        PRE.out.bind(IDLE);
+        WR.in.bind(ACTIVE);
+        WR.out.bind(ACTIVE);
+    }
+
+    void process()
+    {
+        while(true)
+        {
+            wait(10,SC_NS);
+            ACT.fire();
+            wait(10,SC_NS);
+            ACT.fire();
+            wait(10,SC_NS);
+            RD.fire();
+            wait(10,SC_NS);
+            WR.fire();
+            wait(10,SC_NS);
+            PRE.fire();
+            wait(10,SC_NS);
+            ACT.fire();
+            sc_stop();
+        }
+    }
 };
 
 int sc_main(int argc, char* argv[])
 {
-    toplevel t("top");
+    //toplevel t("top");
+    memorybank mb("memorybank");
 
     sc_start();
     return 0;
