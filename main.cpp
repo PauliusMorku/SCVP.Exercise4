81833dadc0fae4275f49dd64e3e12e9062d1cbb0
PauliusMorku <paulius.morku@gmail.com>
2017-12-09 13:24:43 +0100

    Finished multiports part, last commit had big mistake related to adding and removing tokens upon firing

--- main.cpp
+++ main.cpp
@@ -30,47 +30,62 @@ template<unsigned int N=1, unsigned int M=1>
 SC_MODULE(transition)
 {
 public:
-    sc_port<placeInterface> in;
-    sc_port<placeInterface> out;
+    sc_port<placeInterface, N, SC_ALL_BOUND> in;
+    sc_port<placeInterface, M, SC_ALL_BOUND> out;
 
     void fire(void)
     {
+        for (unsigned int i = 0; i < N; i++)
+        {
+            if (in[i]->testTokens() <= 0)
+            {
+                std::cout << this->name() << ": NOT Fired" << std::endl;
+                return;
+            }
+        }
 
-        if (in->testTokens() > 0)
+        for (unsigned int i = 0; i < N; i++)
         {
-            std::cout << this->name() << ": Fired" << std::endl;
-            in->removeTokens(1);
+            in[i]->removeTokens(1);
         }
-        else
+
+        for (unsigned int i = 0; i < M; i++)
         {
-            std::cout << this->name() << ": NOT Fired" << std::endl;
-            out->addTokens(1);
+            out[i]->addTokens(1);
         }
+
+        std::cout << this->name() << ": Fired" << std::endl;
     }
 
     SC_CTOR(transition)
     {
 
     }
-
 };
 
 // TOPLEVEL
 SC_MODULE(toplevel)
 {
     public:
-    transition<1,1> t1, t2;
-    place p1, p2;
+    transition<1,2> t1; // <?,?> passes parameters to transition, <> leaves parameters as defaults
+    transition<2,1> t2;
+    transition<1,1> t3;
+    place p1, p2, p3, p4;
 
-    SC_CTOR(toplevel) : t1("t1"), t2("t2"), p1(1), p2(0)
+
+    SC_CTOR(toplevel) : t1("t1"), t2("t2"), t3("t3"), p1(1), p2(0), p3(0), p4(0)
     {
         SC_THREAD(process);
-        t1.in.bind(p1);
-        t1.out.bind(p2);
-        t2.in.bind(p2);
-        t2.out.bind(p1);
-    }
 
+        t1.in.bind(p1);
+        t1.out.bind(p2);    // 0
+        t1.out.bind(p3);    // 1
+        t2.in.bind(p2);     // 0
+        t2.in.bind(p4);     // 1
+        t2.out.bind(p1);
+        t3.in.bind(p3);
+        t3.out.bind(p4);
+    }
     void process()
     {
         while(true)
@@ -78,19 +93,20 @@ SC_MODULE(toplevel)
             wait(10,SC_NS);
             t1.fire();
             wait(10,SC_NS);
-            t1.fire();
+            t2.fire();
+            wait(10,SC_NS);
+            t3.fire();
             wait(10,SC_NS);
             t2.fire();
             sc_stop();
         }
     }
+
 };
 
 int sc_main(int argc, char* argv[])
 {
     toplevel t("top");
-
-
 
     sc_start();
     return 0;
